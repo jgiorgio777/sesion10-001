25feb713e8b79fd9a2c4c0c295f90d4bb9ad2511
jgiorgio776 <jamesgeorge776@gmail.com>
2022-11-02 21:10:49 -0500

    Se agrega el Proyecto

---
+++ bolaybarra.js
@@ -0,0 +1,6 @@
+for (var i = 1; i <= 1; i++) {
+    document.body.innerHTML += "<marquee behavior='alternate' height='580' SCROLLAMOUNT=10 direction='down'><div id='circulo'></marquee>";
+}
+for (var i = 1; i <= 1; i++) {
+    document.body.innerHTML += "<marquee behavior='alternate' SCROLLAMOUNT=10><div id='cuadrado'></marquee>";
+}
